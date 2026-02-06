17997338cce9196fc20ad4107beacb99a6d1283c
Adam Saber <102896@glr.nl>
2026-02-06 10:56:46 +0100

    Initial portfolio commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/portofolio_mr/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
